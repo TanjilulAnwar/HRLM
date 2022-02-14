75055db9bc5536401f9adfab5d1f777fb70f1f72
tanjilulanwar <tanjilulanwar@gmail.com>
2022-02-14 11:17:36 +0600

    Repository initialized

--- HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/IGenericRepository.cs
@@ -12,8 +12,8 @@ namespace HR.LeaveManagement.Application.Persistence.Contracts
         Task<IReadOnlyList<T>> GetAll();
         Task<T> Add(T entity);
         Task<bool> Exists(int id);
-        Task<T> Update(T entity);
-        Task<T> Delete(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
 
     }
 }
