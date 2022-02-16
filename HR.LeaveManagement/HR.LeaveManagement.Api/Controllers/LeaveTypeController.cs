63f0231060390daee0c0233e8af460d8746bcbce
tanjilulanwar <tanjilulanwar@gmail.com>
2022-02-16 14:02:27 +0600

    API LeaveType Initialized

---
+++ HR.LeaveManagement/HR.LeaveManagement.Api/Controllers/LeaveTypeController.cs
@@ -0,0 +1,28 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    public class LeaveTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveTypeDto>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListQuery());
+            return Ok(leaveTypes);
+        }
+    }
+}
